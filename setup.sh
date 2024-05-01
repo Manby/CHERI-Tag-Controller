e2d4ef2c6936273e6cd383f9341fc0b1ea469f50
Manby <kofiwii@googlemail.com>
2024-05-01 13:23:43 +0100

    Modified setup script to install ChampSim dependencies

--- setup.sh
+++ setup.sh
@@ -5,4 +5,8 @@ git clone git@github.com:ChampSim/ChampSim.git &&
 cp ../CHERI-Tag-Controller/ChampSim-Modified-Files/inc/cache.h ./ChampSim/inc/cache.h &&
 cp ../CHERI-Tag-Controller/ChampSim-Modified-Files/src/main.cc ./ChampSim/src/main.cc &&
 cp ../CHERI-Tag-Controller/ChampSim-Modified-Files/src/champsim.cc ./ChampSim/src/champsim.cc &&
-cd ../CHERI-Tag-Controller
+cd ChampSim &&
+git submodule update --init &&
+vcpkg/bootstrap-vcpkg.sh &&
+vcpkg/vcpkg install &&
+cd ../../CHERI-Tag-Controller
