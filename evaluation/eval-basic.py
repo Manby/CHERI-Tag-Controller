18402745c50704e54ff448a0e40dfa3980036dd3
Manby <kofiwii@googlemail.com>
2024-05-01 19:56:36 +0100

    Implemented eval-capacity.py

--- evaluation/eval-basic.py
+++ evaluation/eval-basic.py
@@ -6,17 +6,27 @@ parser = argparse.ArgumentParser()
 parser.add_argument('-l', dest='workload_list', type=str, required=True)
 parser.add_argument('-n', dest='n', type=str, required=True)
 parser.add_argument('-w', dest='warmup', type=str, required=False, default="0.1")
+parser.add_argument('-m', dest='morello', type=str, required=False, default="no")
 
 parsed_args = parser.parse_args()
 
 workloads = getWorkloads(parsed_args.workload_list)
 
-print("~~~~~ Configuring ChampSim ~~~~~\n")
-champsimConfig(128, 4, None)        # 4-way 32KiB Cache
-print("\n\n")
-
 stats = {}
-schemes = ["baseline", "etm", "morello-t", "morello-z", "phoenix-8t"]
+
+if parsed_args.morello == "yes":
+    schemes = ["morello-t", "morello-z"]
+
+    print("~~~~~ Configuring ChampSim ~~~~~\n")
+    champsimConfig(128, 4, None)        # 4-way 32KiB Cache
+    print("\n\n")
+
+else:
+    schemes = ["baseline", "etm", "phoenix-8t"]
+
+    print("~~~~~ Configuring ChampSim ~~~~~\n")
+    champsimConfig(128, 4, None)        # 4-way 32KiB Cache
+    print("\n\n")
 
 for workload in workloads:
     curr_workload_stats = {}
@@ -25,6 +35,12 @@ for workload in workloads:
 
     print("******************  USING WORKLOAD: " + workload[0] + "  ******************")
     for scheme in schemes:
+        if scheme == "morello-z":
+            # update the cache parameters
+            print("~~~~~ Reconfiguring ChampSim ~~~~~\n")
+            champsimConfig(128, 4, None)        # 4-way 32KiB Cache
+            print("\n\n")
+
         print("##########  EMULATING SCHEME: " + scheme + "  ##########")
         curr_workload_stats[scheme] = doRun(scheme, initial_state, llc_requests,
                                             int(parsed_args.n),
